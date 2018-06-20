a718dc6fb99de57635bada7ae98c678ad2521a93
Adrian Atienza <adrian.atienza.1996@gmail.com>
2018-06-20 11:37:51 +0200

    Correcciones

--- KNeighbors.py
+++ KNeighbors.py
@@ -1,18 +1,25 @@
 import math
+
 class KNeighbors :
     instances = []
     class_num = 0
+    var_names = list()
     k = 0
 
     #Init class with .csv file
     #Archivo -> Path of .csv file
     #Class_num -> Num of class column in .csv file
+
     def __init__(self, archivo, class_num):
         self.class_num = class_num
-
+        #Reading .csv
         with open(archivo, "r") as  datos:
             instance = datos.read().splitlines()
+            #Saving var names
+            self.var_names = instance[0].split(",")
             instance.pop(0)
+
+            #Saving instances 
             for i in instance :
                 self.instances.append(i.split(","))
 
@@ -22,11 +29,14 @@ class KNeighbors :
 
 
     def _distance_(self,tuple1,tuple2):
+        print("Hola")
         distance = 0
         cont = 0
+        print(len(self.var_names))
         while cont < len(self.var_names):
             if cont != self.class_num :
-                distance = distance + pow((tuple1(cont) - tuple2(cont)),2)
+                print(cont)
+                distance = distance + pow((tuple1[cont] - tuple2[cont]),2)
             cont = cont + 1
         distance = math.sqrt(distance)
         return distance
